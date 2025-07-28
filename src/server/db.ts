4c5d5020012c4dfee94bb853e5e1b32f0ff804ec
mirafgan <mirefqan1999@gmail.com>
2025-07-28 18:05:24 +0400

    Integrate Prisma `driverAdapters` preview feature and add `@prisma/adapter-pg` for PostgreSQL support.

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,11 +1,14 @@
 import {PrismaClient} from "@prisma/client";
 
 import {env} from "@/env";
+import {PrismaPg} from '@prisma/adapter-pg'
 
+const adapter = new PrismaPg({connectionString: env.DATABASE_URL})
 const createPrismaClient = () =>
     new PrismaClient({
         log:
             env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+        adapter
     });
 
 const globalForPrisma = globalThis as unknown as {
