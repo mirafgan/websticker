79557dd0366b6c87121ae1c58334cfb6194428c3
mirafgan <mirefqan1999@gmail.com>
2025-08-14 14:22:05 +0400

    Update database environment variable name and integrate Prisma Accelerate

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,15 +1,14 @@
 import {PrismaClient} from "@/generated/prisma/client";
 
 import {env} from "@/env";
-import {PrismaPg} from '@prisma/adapter-pg'
+import {withAccelerate} from "@prisma/extension-accelerate";
 
-const adapter = new PrismaPg({connectionString: env.DATABASE_URL})
+// const adapter = new PrismaPg({connectionString: env.DATABASE_PRISMA_DATABASE_URL})
 const createPrismaClient = () =>
     new PrismaClient({
         log:
             env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-        adapter
-    });
+    }).$extends(withAccelerate());
 
 const globalForPrisma = globalThis as unknown as {
     prisma: ReturnType<typeof createPrismaClient> | undefined;
